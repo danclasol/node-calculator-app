d80b79a93b29d0862a56af5a0527ede60b303374
Daniel <danielclark.projects@gmail.com>
2023-02-09 12:01:35 +0100

    Operacion y resultado por pantalla

--- src/constants/operators.js
+++ src/constants/operators.js
@@ -1,1 +1,5 @@
-export const OPERATORS = ['+', '-', '*', '/', '^', 'log', 'sqrt'];
+export const BINARY_OPERATORS = ['+', '-', '*', '/', '^'];
+
+export const SINGLE_OPERATORS = ['log', 'sqrt'];
+
+export const ALL_OPERATORS = [...BINARY_OPERATORS, ...SINGLE_OPERATORS];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import { OPERATIONS } from '#Constants/operations.js';
+import { BINARY_OPERATORS } from '#Constants/operators.js';
 import { InvalidInputError } from '#Errors/invalidInputError.js';
+import {
+	getBinaryOperatings,
+	getSingleOperatings,
+} from '#Lib/getOperatings.js';
 import { getOperator } from '#Lib/getOperator.js';
 import { promptQuestion } from '#Lib/promptQuestion.js';
 
@@ -16,6 +22,26 @@ import { promptQuestion } from '#Lib/promptQuestion.js';
 		const operator = getOperator(standarizeInput);
 
 		if (!operator) throw new InvalidInputError();
+
+		const splittedInput = standarizeInput.split(operator);
+
+		let firstOperating, secondOperating;
+
+		if (BINARY_OPERATORS.includes(operator)) {
+			[firstOperating, secondOperating] = getBinaryOperatings(splittedInput);
+		} else {
+			[firstOperating] = getSingleOperatings(splittedInput);
+		}
+
+		// 3º Realizar la operación
+		const result = OPERATIONS[operator](firstOperating, secondOperating);
+
+		const roundedResult = Number(Math.round(result + 'e+5') + 'e-5');
+
+		// 4º Mostrar resultado por consola
+		if (isNaN(roundedResult) || !isFinite(roundedResult))
+			console.log('Operacion no válida');
+		else console.log(roundedResult);
 	} catch (error) {
 		if (error instanceof InvalidInputError) console.log(error.message);
 		else
@@ -23,8 +49,4 @@ import { promptQuestion } from '#Lib/promptQuestion.js';
 				`Error no controlado: ${error.message}. Stack: ${error.stack}`
 			);
 	}
-
-	// 3º Realizar la operación
-
-	// 4º Mostrar resultado por consola
 })();
